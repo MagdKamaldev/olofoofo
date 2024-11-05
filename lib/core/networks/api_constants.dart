b59a821724032969addd70d8f537ecf6f0227e3d
magd <magddevf@gmail.com>
2024-11-05 16:29:04 +0200

    testing 1.0.6

--- lib/core/networks/api_constants.dart
+++ lib/core/networks/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String apiBASEURL = "http://192.168.89.188:8080/api/v1/";
+  static const String apiBASEURL = "http://192.168.1.74:8080/api/v1/";
   static const String signUp = "auth/signup";
   static const String login = "auth/login";
   static const String getUser = "users/me";
